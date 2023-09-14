ac76318bdc9be6a32d84d5cc0f2c7485a097606b
likith19981211 <sailikithgowd1998@gmail.com>
2023-09-14 12:15:32 +0530

    webclient-configuration

--- src/main/java/com/ecommerce/orderservice/service/OrderService.java
+++ src/main/java/com/ecommerce/orderservice/service/OrderService.java
@@ -8,6 +8,7 @@ import com.ecommerce.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -20,6 +21,8 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
+    private final WebClient webClient;
+
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -30,7 +33,21 @@ public class OrderService {
                                         .toList();
 
         order.setOrderLineItemsList(orderLineItems);
-        orderRepository.save(order);
+
+        //call inventory service and place order if product is
+        // in stock
+
+        Boolean result = webClient.get()
+                        .uri("http://localhost:8082/api/inventory")
+                                .retrieve()
+                                        .bodyToMono(Boolean.class)
+                                                .block();
+        if (result) {
+            orderRepository.save(order);
+        }else {
+            throw new IllegalArgumentException("The product that you are looking for is not in stock, please check our other products");
+        }
+
 
     }
 
